d5c3f2d3f192f5a59b291e517afe1d0bf72c8a18
logpup <jyjprk@gmail.com>
2021-02-15 03:48:05 -0500

    slideshow v 1

--- test.js
+++ test.js
@@ -1,19 +1,4 @@
 $("document").ready(function(){
-
-    // Text Toggle for Long List Items in Main-Menu
-    function toggleText(elem, init, second) {
-        $(elem).mouseover(function(){
-            $(elem).text(second);
-        })
-        $(elem).mouseout(function(){
-            $(elem).text(init);
-        })
-    }
-
-    toggleText("#bc-screening > a", "At-Home B.C. Screening", "At-Home Breast Cancer Screening Device");
-    toggleText("#assist-tech > a", "Assist-Tech High Spinal Injuries", "Assistive Technology for High Spinal Cord Injuries");
-    toggleText("#non-invasive > a", "Non-Invas. V.N. Stimulation", "Non-Invasive Vagus Nerve Stimulation Device");
-
     let slideNum = 5;
     
     let i = 0;
@@ -94,5 +79,4 @@ $("document").ready(function(){
             });
         }
     }
-    
-})
+});
