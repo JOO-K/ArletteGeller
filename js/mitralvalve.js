8ddf7d9cee497a1d461b4c908e5cd6b7f1824190
JOO-K <68034507+JOO-K@users.noreply.github.com>
2021-04-02 14:50:19 -0700

    min

--- js/mitralvalve.js
+++ js/mitralvalve.js
@@ -1,3 +1,100 @@
+<<<<<<< HEAD
+//Variables for setup
+
+let container;
+let camera;
+let renderer;
+let scene;
+
+
+function init() {
+  container = document.getElementById( 'canvas' );
+  // document.body.appendChild( container );
+    
+  //Create scene
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color('#ebebeb');
+
+  //Camera setup    
+  const fov = 35;
+  const aspect = container.clientWidth * 1.0/ container.clientHeight;
+  const near = 0.1;
+  const far = 1000;
+
+  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(6, 5, 5);
+  camera.lookAt(0, 0, 0);    
+    
+    
+  //GridVis
+    
+    const size = 10;
+    const divisions = 10;
+    const colorGrid = '#ff0065';
+
+    const gridHelper = new THREE.GridHelper( size, divisions, colorGrid );
+    scene.add( gridHelper );
+    grid = gridHelper;
+ 
+  
+  //Light setup
+
+  const light3= new THREE.AmbientLight(0xffffff, .4);
+  light3.position.set(0,0,0);
+  scene.add(light3);    
+    
+  const light = new THREE.DirectionalLight(0xffffff, .5);
+  light.position.set(0, 100, 100);
+  scene.add(light);      
+    
+  const light2 = new THREE.DirectionalLight(0xffffff, .5);
+  light2.position.set(0, -100, -100);
+  scene.add(light2);        
+    
+  //Renderer
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setSize(container.clientWidth * 1.0, container.clientHeight * 1.0);
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  container.appendChild(renderer.domElement);
+
+  //Load Models
+  let loader = new THREE.GLTFLoader();
+    
+  loader.load("obj/mitralvalve.gltf", function(gltf) {
+    scene.add(gltf.scene);
+    mitral = gltf.scene;
+    animate();
+  });
+    
+}
+
+
+function animate() {
+    
+    
+  grid.position.set(0,-2,0);
+  mitral.position.set(0,-2,0);    
+    
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+    
+}
+
+init();
+
+function onWindowResize() {
+  camera.aspect = container.clientWidth * 1.0/ container.clientHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(container.clientWidth * 1.0, container.clientHeight);
+}
+
+window.addEventListener("resize", onWindowResize);
+
+//Orbit Controls
+  controls = new THREE.OrbitControls( camera, renderer.domElement);
+=======
 //Variables for setup
 
 let container;
@@ -89,3 +186,4 @@ window.addEventListener("resize", onWindowResize);
 
 //Orbit Controls
   controls = new THREE.OrbitControls( camera, renderer.domElement);
+>>>>>>> c2a319dda22ff585d3c39e978d41a139f91b0566
